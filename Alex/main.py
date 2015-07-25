1abfa4ef7e1edec908d239b5086467fb58db684f
alex.cosma <alexx.cosma@gmail.com>
2015-07-25 18:47:56 +0300

    - added short script to compute crime pattern per months

--- Alex/main.py
+++ Alex/main.py
@@ -1,6 +1,9 @@
 __author__ = 'Amalia'
 
 # read config file to get path of dataset
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as df
 
 from enum import Enum
 class Dataset(Enum):
@@ -26,3 +29,14 @@ if __name__ == "__main__":
     print getSamplePath()
     print getTrainPath()
     print getTestPath()
+
+    train = pd.read_csv(getTrainPath(), parse_dates=['Dates'])
+    train['DayOfYear'] = train['Dates'].map(lambda x: x.strftime("%m-%d"))
+    df = train[['Category','DayOfYear']].groupby(['DayOfYear']).count()
+    df.plot(y='Category', label='Number of events', figsize=(15,10))
+    plt.title("Crimes occur with a regular pattern: two peaks per month")
+    plt.ylabel('Number of crimes')
+    plt.xlabel('Day of year')
+    plt.grid(True)
+
+    plt.savefig('Distribution_of_Crimes_by_DayofYear.png')
