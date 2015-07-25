4275a4d5f3e53e06c32c78fb32afa760b1537078
alex.cosma <alexx.cosma@gmail.com>
2015-07-25 18:41:43 +0300

    - added convenience methods for reading csv files

--- Alex/main.py
+++ Alex/main.py
@@ -2,12 +2,27 @@ __author__ = 'Amalia'
 
 # read config file to get path of dataset
 
-def getPathOfDataset():
+from enum import Enum
+class Dataset(Enum):
+    sample = 'sample'
+    train = 'train'
+    test = 'test'
+
+def getPath(dataset):
     import ConfigParser
     config = ConfigParser.ConfigParser()
     config.read('properties.cfg')
-    path = config.get("Properties", "datasetPath")
+    path = config.get("Properties", dataset.value)
     return path
 
+def getSamplePath():
+    return getPath(Dataset.sample)
+def getTestPath():
+    return getPath(Dataset.test)
+def getTrainPath():
+    return getPath(Dataset.train)
+
 if __name__ == "__main__":
-    print getPathOfDataset()
+    print getSamplePath()
+    print getTrainPath()
+    print getTestPath()
