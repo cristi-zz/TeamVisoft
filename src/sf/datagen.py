88f4c02da175791681092bd6be48c163fe6d3c53
Cristi <@>
2015-07-26 13:40:13 +0300

    Skeleton for data generation

---
+++ src/sf/datagen.py
@@ -0,0 +1,24 @@
+__author__ = 'visoft'
+
+"""
+This file handles data generation.
+Each routine should declare its own constant and check if the file already exists.
+
+Ex: Take a sample out of train data
+1) Create a constant TRAIN_DATA_SAMPLE_RANDOM_01 with a proprer file name
+2) create a function that:
+2.1 check if the file exists in input_dir. If exisits, exit
+2.2 call the code that generates the function
+2.3 save the data in out_dir
+
+The same pattern should be repeated for all the importnat files. So when smb comes back after a while,
+runs the datagen locally and he has all the recent developments.
+
+
+"""
+
+
+#TODO: Take first 10-20K instances from the training set and save them. They will be used to test our feature extraction functions
+# Save for now as a csv or sth, we'll see later on more advanced formats.
+
+#TODO: Don't hack it, pull out functions that might be common (check file existence, etc)
