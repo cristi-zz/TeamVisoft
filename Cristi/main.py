352d4a2cd23c537394045364ec1fccb44e9da4e2
Cristian Vicas <cristian.vicas@catalysts.cc>
2015-08-01 20:33:26 +0300

    Finished all the steps. An predicted output file is generated

--- Cristi/main.py
+++ Cristi/main.py
@@ -15,17 +15,31 @@ if __name__ == "__main__":
     test_file_name =fh.get_src_path(fh.TEST_FILE_NAME)
     test_set = pd.read_csv(test_file_name)
 
-    X,Y = basic.get_X_Y_from_dataframe(train_set)
+    X,Y,class_labels = basic.get_X_Y_class_labels_from_dataframe(train_set)
     T = basic.get_X_from_dataframe(test_set)
     Id = basic.get_Id_from_test_dataframe(test_set)
 
 
     #Get a classifier. Later, it will be a pipeline
     pipeline = bayes.GaussianNB()
+
+    #TODO tune pipeline metaparameters.
+    #The most time consuming part! We have to be really creative here!
+
+
+    #Once the metaparameters were found, set them and fit ("learn") the training set
     pipeline.fit(X,Y)
 
-    predicted = pipeline.predict_proba(T)
-    classes = pipeline.classes_
+    #Sometimes a little time consuming but not very much
+    predicted_proba = pipeline.predict_proba(T)
+
+    #Build nicely the pandas so we can write a nice csv
+    output = basic.build_pandas_output(Id,predicted_proba,class_labels)
+
+    output_file_name = fh.get_out_path("cv_Prediction_Attempt_01.08.2015.csv")
+    output.to_csv(output_file_name,index=False)
+
+    #Write the file, ready to be submitted
     print "Done"
 
 
--- src/sf/basic.py
+++ src/sf/basic.py
@@ -4,21 +4,30 @@ Basic data manipulation
 
 """
 
+import pandas as pd
+
 NON_NUMERIC  = ['Id','Dates','Descript','PdDistrict','DayOfWeek','Resolution','Address']
 CLASS_COLUMN = 'Category'
 
 
-def get_X_Y_from_dataframe(dataframe,features_to_remove = None, target_column = None):
+def get_X_Y_class_labels_from_dataframe(dataframe,features_to_remove = None, target_column = None):
     if features_to_remove is None:
         features_to_remove = NON_NUMERIC
     if target_column is None:
         target_column = CLASS_COLUMN
 
-    Y = dataframe[target_column].values
+    #Get sorted class labels and in Y use only indexes
+
+    class_labels = dataframe[target_column].unique()
+    class_labels.sort()
+    class_labels_dict = {class_labels[i]:i for i in range(len(class_labels)) }
+    Y_lables = dataframe[target_column]
+    Y = Y_lables.map(lambda x: class_labels_dict[x])
+
     dataframe = dataframe.drop(target_column,axis=1,errors='ignore')
 
     X = get_X_from_dataframe(dataframe,features_to_remove)
-    return X, Y
+    return X, Y,class_labels
 
 def get_X_from_dataframe(dataframe,features_to_remove = None):
     """
@@ -36,3 +45,11 @@ def get_X_from_dataframe(dataframe,features_to_remove = None):
 def get_Id_from_test_dataframe(dataframe):
     Id = dataframe['Id'].values
     return Id
+
+def build_pandas_output(id,predicted,class_labels):
+    output = pd.DataFrame({'Id':id})
+    for i in range(predicted.shape[1]):
+        column = predicted[:,i]
+        key = class_labels[i]
+        output[key] = column
+    return output
