723b5c1eb65777e9416085fec614803f3f65fd36
Diana Dersedan <diana.dersedan@gmail.com>
2015-08-19 16:09:42 +0300

    Added predict_proba method and test for it

--- src/sf/knn.py
+++ src/sf/knn.py
@@ -2,6 +2,7 @@ __author__ = 'Diana'
 
 import math
 import collections
+import numpy as np
 
 #distance between p1 and p2
 def getDistance( p1, p2 ):
@@ -41,28 +42,50 @@ class KNeighborsClassifier():
         sorted_indexes = sorted(range(len(distances)), key=lambda k: distances[k])
         for index in range(0, self.n_neighbors):
             indices.append(sorted_indexes[index])
-        return distances, indices
+        return np.array(distances), np.array(indices)
 
     #returns the predicted values list for the transmitted value
     def predict(self, val):
         predicted = []
         for elem in val:
             distances, indices = self.kneighbors(elem)
-            #used to store the frequency of each target value found in neighbors
-            #collections - used in order to keep the data structure ordered
-            frequencies = collections.OrderedDict()
-            for index in indices:
-                if frequencies.has_key(self.y[index]):
-                    frequencies[self.y[index]] += 1
-                else:
-                    frequencies[self.y[index]] = 0
-
+            frequencies = self.getFrequenciesOfTargetValues(indices)
             #sort target values by frequency
             frequencies = sorted(frequencies.items(), key=lambda x: x[1])
             #add most frequent target value to the result
             predicted.append(frequencies[-1][0])
-        return predicted
+        return np.array(predicted)
 
+    def getFrequenciesOfTargetValues(self, indices):
+        #used to store the frequency of each target value found in neighbors
+        #collections - used in order to keep the data structure ordered
+        frequencies = collections.OrderedDict()
+        for index in indices:
+            if frequencies.has_key(self.y[index]):
+                frequencies[self.y[index]] += 1
+            else:
+                frequencies[self.y[index]] = 1
+        return frequencies
+
+    def predict_proba(self, val):
+        predict_proba = []
+        #get unique target values
+        target_values = set(self.y)
+        for elem in val:
+            predict_proba_aux = []
+            distances, indices = self.kneighbors(elem)
+            frequencies = self.getFrequenciesOfTargetValues(indices)
+            for target_value in target_values:
+                found = 0
+                for value in frequencies.items():
+                    if target_value == value[0]:
+                        found = 1
+                        predict_proba_aux.append(float(value[1])/self.n_neighbors)
+                        break
+                if found == 0:
+                    predict_proba_aux.append(0.0)
+            predict_proba.append(predict_proba_aux)
+        return np.array(predict_proba)
 
 
 
--- tests/test_knn.py
+++ tests/test_knn.py
@@ -30,3 +30,21 @@ def test_custom_knn():
     custom_result = classifier.predict(x_test)
 
     assert np.array_equal(result, custom_result)
+
+def test_predict_proba():
+    iris = dts.load_iris()
+    X = iris.data[:, :2]
+    y = iris.target
+    x_test = np.array([[5., 3.5], [5., 3.2], [5.1, 3.5]])
+
+    neigh = ngh.KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(X, y)
+    result = neigh.predict_proba(x_test)
+
+    classifier = pipe.Pipeline([
+        ('clf', knn.KNeighborsClassifier(n_neighbors=3))
+    ])
+    classifier.fit(X, y)
+    custom_result = classifier.predict_proba(x_test)
+
+    assert np.array_equal(result, custom_result)
